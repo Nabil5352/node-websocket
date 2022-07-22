8a924749551e9c1aed0b76ec3f4d6daab6458f8a
nabil5352 <nabil.ibn.mahmud@gmail.com>
2022-07-23 01:35:17 +0600

    Initial implementatin

---
+++ README.md
@@ -0,0 +1,5 @@
+###
+Websocket implementation with Node.js
+Node version used: 14.15.3
+NPM version used: 6.14.9
+NPM package for websocket: 
---
+++ index.js
@@ -0,0 +1,55 @@
+const http = require("http");
+const WebSocketServer = require("websocket").server;
+
+const httpServer = http.createServer((request, response) => {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+})
+
+let connection = null;
+
+httpServer.listen(8080, () =>     console.log((new Date()) + ' Server is listening on port 8080'))
+
+const wsServer = new WebSocketServer({
+    httpServer: httpServer,
+    // You should not use autoAcceptConnections for production
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    setInterval(sendEvery5seconds, 5000);
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
+
+function sendEvery5seconds() {
+    connection.send(`Message ${Math.random()}`);
+}
+
